087452c55e316cc7e91322abb4fd787b25b939b3
methodmenon <methodmenon@gmail.com>
2014-08-11 19:44:21 -0700

    Commit 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function(){
+	//random number variable
+	var random_number = Math.floor((Math.random() * 100) + 1);
+	console.log(random_number);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -18,54 +21,42 @@ $(document).ready(function(){
   	});
 
   	
-  	var user_guess = $("#userGuess").val();
+  	//var user_guess = $("#userGuess").val();
   	
   	$("#guessButton").click(function(e){
   		e.preventDefault(e);
-  		console.log(user_guess);
-  		temp(+user_guess);
+  		var user_guess = $("#userGuess").val();
+  		hotOrCold(user_guess);
 
   	});
 
-});
+  	var hotOrCold = function(x) {
+	if ((x > 0) && (x < 100)) {
 
-//function for deciding how close my number is to actual
-var temp = function(x) {
-	while ((x > 0) && (x < 100)) {
-
-		//return a random number between 1 and 100
-		//Math.random() returns a random number between 0 and 1
-		//Math.floor(x) returns x, rounded DOWN to the nearest integer
-		var random_number = Math.floor((Math.random() * 100) + 1);
-		console.log(random_number);
-		//find absolute value of difference between random value and user's input
+		
 		var abs_diff = Math.abs(x - random_number);
 		if (abs_diff == 0)
 		{
-			
-			//document.getElementById("#feedback").innerHTML = "Correct!";
 			$("#feedback").html("Correct!");
 		
-			//return("Correct!");
 		}
 		else if (abs_diff > 50)
 		{
-			//document.getElementById("#feedback").innerHTML = "Very Cold!";
 			$("#feedback").html("Very Cold!");
-			//return("Very Cold!");
+			
 		}
 		else if (abs_diff > 20)
 		{
-			//document.getElementById("#feedback").innerHTML = "Cold!";
 			$("#feedback").html("Cold");
-			//return("Cold");
 		}
 		else
 		{
-			//document.getElementById("#feedback").innerHTML = "Very Warm!";
 			$("#feedback").html("Very Warm!");
-			//if abs_diff <=5
-			//return("Very Warm!");
 		}
 	}
-}
+};
+
+});
+
+
+
