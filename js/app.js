cbb4efce02b1602530de668aa101d0a8ab99f022
methodmenon <methodmenon@gmail.com>
2014-08-16 22:00:07 -0700

    Full working version of application. Also deleted teh .gitattributes file

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	//random number variable
 	var random_number = Math.floor((Math.random() * 100) + 1);
 	console.log(random_number);
+	var num_guess = 0;
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -25,34 +26,61 @@ $(document).ready(function(){
   	
   	$("#guessButton").click(function(e){
   		e.preventDefault(e);
+  		//console.log(num_guess);
+  		num_guess++;
   		var user_guess = $("#userGuess").val();
   		hotOrCold(user_guess);
+  		$("#count").html(num_guess);
+  		$("#guessList").append("<li>" + user_guess + "</li>");
 
   	});
 
   	var hotOrCold = function(x) {
-	if ((x > 0) && (x < 100)) {
+  	//type of x is currently string, need to try to convert it into an integer
+  	x = parseInt(x);
+  	//make sure x is an integer
+  	if ((typeof x == "number") && Math.floor(x) == x)
+  	{
+		if ((x > 0) && (x < 100)) {
 
 		
-		var abs_diff = Math.abs(x - random_number);
-		if (abs_diff == 0)
-		{
-			$("#feedback").html("Correct!");
+			var abs_diff = Math.abs(x - random_number);
+			if (abs_diff == 0)
+			{
+				$("#feedback").html("Correct!");
 		
-		}
-		else if (abs_diff > 50)
-		{
-			$("#feedback").html("Very Cold!");
-			
-		}
-		else if (abs_diff > 20)
-		{
-			$("#feedback").html("Cold");
+			}
+			else if (abs_diff > 50)
+			{
+				$("#feedback").html("Very Cold!");
+				$("#userGuess").val("");
+			}
+			else if (abs_diff > 20)
+			{
+				$("#feedback").html("Cold");
+				$("#userGuess").val("");
+			}
+			else if (abs_diff > 5)
+			{
+				$("#feedback").html("Warm!");
+				$("#userGuess").val("");
+			}
+			else
+			{
+				$("#feedback").html("Very Warm!");
+				$("#userGuess").val("");
+			}
 		}
 		else
 		{
-			$("#feedback").html("Very Warm!");
+			alert("Please enter an integer between 1 and 100!");
+			window.location.reload();
 		}
+	}
+	else
+	{
+		alert("Please enter an integer between 1 and 100!");
+		window.location.reload();
 	}
 };
 
